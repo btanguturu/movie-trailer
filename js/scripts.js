b43190698a5b364ea6b78c27f7a7a6c7f5e0ba4c
smilenlaugh96 <36969266+smilenlaugh96@users.noreply.github.com>
2019-05-19 18:08:23 -0700

    Content
    *added video
    *added some styling things
    *updated favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log('OK');
+const player = new Plyr('#player', {
+
+    controls: ['play', 'progress', 'fullscreen', 'current-time', 'volume', 'settings'],
+    muted: false,
+    duration: 10,
+
+
+
+
+});
