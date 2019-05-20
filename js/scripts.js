69ae5deb9bfc296ed04cf2ac9c672937d2075fc1
smilenlaugh96 <36969266+smilenlaugh96@users.noreply.github.com>
2019-05-19 21:15:23 -0700

    removed settings

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,5 @@ const player = new Plyr('#player', {
 
     controls: ['play', 'progress', 'fullscreen', 'current-time', 'volume', 'settings'],
     muted: false,
-    setting: ['quality', 'speed', 'loop'],
 
 });
